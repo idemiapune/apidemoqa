db73b4e61760bb8cbd10332f4365a563cf34081f
riya <riya@10.72.6.133>
2018-05-08 17:41:57 +0530

    adding properties of book

--- src/main/java/in/pune/amanora/idemia/model/Book.java
+++ src/main/java/in/pune/amanora/idemia/model/Book.java
@@ -1,5 +1,38 @@
 package in.pune.amanora.idemia.model;
 
 public class Book {
+	private long id;
+	private String name;
+	private String author;
+	private int noofpages;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getNoofpages() {
+		return noofpages;
+	}
+	public void setNoofpages(int noofpages) {
+		this.noofpages = noofpages;
+	}
+	
+	
+	
+	
+	
 
 }
