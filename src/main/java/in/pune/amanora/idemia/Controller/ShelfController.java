8acf3faa0581896e4322020ef4c62bb4a54b337b
shraddha <shraddha@10.200.161.7>
2018-05-10 16:55:40 +0530

    Added @RestController annotation

--- src/main/java/in/pune/amanora/idemia/Controller/ShelfController.java
+++ src/main/java/in/pune/amanora/idemia/Controller/ShelfController.java
@@ -4,13 +4,15 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import in.pune.amanora.idemia.model.Shelf;
 
+@RestController
 public class ShelfController {
 
 	@RequestMapping(method = RequestMethod.GET, value = "/shelves")
-	public List<Shelf> getShelf() {
+	public List<Shelf> getShelves() {
 		return RackController.rack.getShelves();
 	}
 
