5eb4b98057d266a5110d352bde72b6991f1a35b9
shraddha <shraddha@10.200.161.7>
2018-05-10 16:06:23 +0530

    Removed Static Shelf and changed return value inside GET request method.

--- src/main/java/in/pune/amanora/idemia/Controller/ShelfController.java
+++ src/main/java/in/pune/amanora/idemia/Controller/ShelfController.java
@@ -1,5 +1,7 @@
 package in.pune.amanora.idemia.Controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -7,11 +9,9 @@ import in.pune.amanora.idemia.model.Shelf;
 
 public class ShelfController {
 
-	public static Shelf shelf = new Shelf();
-
 	@RequestMapping(method = RequestMethod.GET, value = "/shelves")
-	public Shelf getShelf() {
-		return shelf;
+	public List<Shelf> getShelf() {
+		return RackController.rack.getShelves();
 	}
 
 }
