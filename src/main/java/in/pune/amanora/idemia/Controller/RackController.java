6b09de3766a5d4a50bc1fd2d7c2834a60952c3de
shraddha <shraddha@10.72.6.136>
2018-05-14 12:49:43 +0530

    Added POST API call which will add rack details.

--- src/main/java/in/pune/amanora/idemia/Controller/RackController.java
+++ src/main/java/in/pune/amanora/idemia/Controller/RackController.java
@@ -1,7 +1,8 @@
 package in.pune.amanora.idemia.Controller;
 
-
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.pune.amanora.idemia.model.Rack;
@@ -9,11 +10,34 @@ import in.pune.amanora.idemia.model.Rack;
 @RestController
 public class RackController {
 
-	static Rack rack = new Rack();
+	public static Rack rack = new Rack();
 
+	/*
+	 * Get Rack details
+	 * 
+	 * @author Shraddha
+	 */
 	@RequestMapping(value = "/rack")
 	public Rack getRack() {
+
 		return rack;
+	}
+
+	/*
+	 * Add new Rack
+	 * 
+	 * @RequestBody Rack object
+	 * 
+	 * @author Shraddha
+	 */
+	@RequestMapping(method = RequestMethod.POST, value = "/rack")
+	public Rack addRack(@RequestBody Rack rack) {
+		if (RackController.rack.getId() == 0 ) {
+			RackController.rack.setId(rack.getId());
+			RackController.rack.setNoOfShelves(rack.getNoOfShelves());
+			RackController.rack.setShelves(rack.getShelves());
+		}
+		return RackController.rack;
 	}
 
 }
