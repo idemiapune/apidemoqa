e72178fc7e3d8c3168a4d077f4e246aa9b9a82b9
shraddha <shraddha@10.72.6.209>
2018-05-15 12:38:51 +0530

    added PUT API call for updating book properties.

--- src/main/java/in/pune/amanora/idemia/Controller/BookController.java
+++ src/main/java/in/pune/amanora/idemia/Controller/BookController.java
@@ -13,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BookController {
 
-	/*Get books in a particular shelf
+	/*
+	 * Get books in a particular shelf
+	 * 
 	 * @Path variable 'id' : for shelf id
+	 * 
 	 * @Author : Swarshri
 	 */
 	@RequestMapping(method = RequestMethod.GET, value = "/shelves/{id}/books")
@@ -27,21 +30,22 @@ public class BookController {
 		return null;
 	}
 
-	/*Get a particular book details in a particular shelf.
+	/*
+	 * Get a particular book details in a particular shelf.
+	 * 
 	 * @ Two Path variable needed for shelfid 'id' & bookid 'bookid'
+	 * 
 	 * @Author : Swarshri
 	 */
-	
+
 	@RequestMapping(method = RequestMethod.GET, value = "/shelves/{id}/books/{bookid}")
-	public Book getBookDetails(@PathVariable("id") long id,@PathVariable("bookid") long bookid) {
+	public Book getBookDetails(@PathVariable("id") long id, @PathVariable("bookid") long bookid) {
 		for (int i = 0; i < RackController.rack.getShelves().size(); i++) {
 
 			if (RackController.rack.getShelves().get(i).getId() == id) {
-				for (int j = 0; j < RackController.rack.getShelves().get(i)
-						.getBooks().size(); j++) {
+				for (int j = 0; j < RackController.rack.getShelves().get(i).getBooks().size(); j++) {
 
-					if (RackController.rack.getShelves().get(i).getBooks()
-							.get(j).getId() == bookid) {
+					if (RackController.rack.getShelves().get(i).getBooks().get(j).getId() == bookid) {
 						return RackController.rack.getShelves().get(i).getBooks().get(j);
 					}
 				}
@@ -51,10 +55,12 @@ public class BookController {
 		return null;
 
 	}
-	
+
 	/*
 	 * Below API is for adding new Book in a shelf
+	 * 
 	 * @RequestBody Book object
+	 * 
 	 * @author Riya
 	 */
 	@RequestMapping(method = RequestMethod.POST, value = "/shelves/{id}/books")
@@ -66,5 +72,42 @@ public class BookController {
 			}
 		}
 		return book;
+	}
+
+	/*
+	 * below API is for updating book properties
+	 * 
+	 * two @PathVariable is given 1st(id) is for shelf and 2nd(bookid) is for Book
+	 * 
+	 * @author Shraddha
+	 */
+	@RequestMapping(method = RequestMethod.PUT, value = "/shelves/{id}/books/{bookid}")
+	public Book updateBook(@PathVariable("id") long id, @RequestBody Book book, @PathVariable("bookid") long bookid) {
+		for (int i = 0; i < RackController.rack.getShelves().size(); i++) {
+
+			if (RackController.rack.getShelves().get(i).getId() == id) {
+				for (int j = 0; j < RackController.rack.getShelves().get(i).getBooks().size(); j++) {
+
+					if (RackController.rack.getShelves().get(i).getBooks().get(j).getId() == bookid) {
+						if ((!book.getAuthor().equals(null))) {
+							RackController.rack.getShelves().get(i).getBooks().get(j).setAuthor(book.getAuthor());
+						}
+						if ((!book.getName().equals(null))) {
+							RackController.rack.getShelves().get(i).getBooks().get(j).setName(book.getName());
+						}
+						if ((book.getId() != 0)) {
+							RackController.rack.getShelves().get(i).getBooks().get(j).setId(book.getId());
+						}
+						if ((book.getNoOfPages() != 0)) {
+							RackController.rack.getShelves().get(i).getBooks().get(j).setNoOfPages(book.getNoOfPages());
+						}
+					}
+				}
+				return book;
+			}
+
+		}
+		return null;
+
 	}
 }
