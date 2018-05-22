b0a9cf8ba5191e06df5ea591f6b5d85fab7f0065
shraddha <shraddha@10.72.6.132>
2018-05-22 11:17:30 +0530

    Changing name to common exception

--- src/main/java/in/pune/amanora/idemia/util/ErrorMessage.java
+++ src/main/java/in/pune/amanora/idemia/util/ErrorMessage.java
@@ -3,4 +3,5 @@ package in.pune.amanora.idemia.util;
 public class ErrorMessage {
 	public static final String SHELF_NOT_FOUND = "Shelf not found with Id Provied";
 	public static final String NO_SHELF = "No Shelf is present inside Rack";
+	public static final String Dublicate_Shelf="Shelf id is already present";
 }
